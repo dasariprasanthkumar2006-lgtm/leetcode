131e6d1de226603b10b7dfb28ed0548d7e52796a
dasariprasanthkumar2006-lgtm <dasariprasanthkumar2006@gmail.com>
2026-07-20 13:26:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (70.72%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.c
@@ -0,0 +1,17 @@
+
+int* getRow(int rowIndex, int* returnSize) {
+    *returnSize = rowIndex + 1;
+
+    int *row = (int *)malloc((rowIndex + 1) * sizeof(int));
+
+    row[0] = 1;
+
+    for (int i = 1; i <= rowIndex; i++) {
+        row[i] = 1;
+        for (int j = i - 1; j > 0; j--) {
+            row[j] = row[j] + row[j - 1];
+        }
+    }
+
+    return row;
+}
