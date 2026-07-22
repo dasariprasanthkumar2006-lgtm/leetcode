140cd73bd9e27f94baa44ca7473864c076e2b439
dasariprasanthkumar2006-lgtm <dasariprasanthkumar2006@gmail.com>
2026-07-22 13:09:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (76.41%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def diagonalSum(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: int
+        """
+        n = len(mat)
+        total = 0
+
+        for i in range(n):
+            # Primary diagonal
+            total += mat[i][i]
+
+            # Secondary diagonal
+            if i != n - 1 - i:
+                total += mat[i][n - 1 - i]
+
+        return total
