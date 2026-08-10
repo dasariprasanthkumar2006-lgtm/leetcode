5e2bf9fccfe99f2a3750ca14e47d6218e6743771
dasariprasanthkumar2006-lgtm <dasariprasanthkumar2006@gmail.com>
2026-08-10 10:56:50 +0530

    Time: 14 ms (31.52%) | Memory: 13.1 MB (99.28%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in range(0,len(nums)):
+            if i!=nums[i]:
+                return i
+
+        return nums[-1]+1
+            
